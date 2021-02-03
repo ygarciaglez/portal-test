8ec0a321f679e692330c9c31cf85b91f387c436e
Yordy Garcia <ygarciaglez1995@gmail.com>
2021-02-02 19:29:41 -0600

    sssssssscxcsddds

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"/portal"}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
