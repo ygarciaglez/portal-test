5a940f45f89767a748b23f6d1601919c93860b6f
Yordy Garcia <ygarciaglez1995@gmail.com>
2021-02-04 11:42:50 -0600

    aa

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>,
   document.getElementById('root')
